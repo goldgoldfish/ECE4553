ae93b2fd35f701dacbea933d8d75cff48118ed5f
Unknown <ben.wedemire@gmail.com>
2018-11-11 14:10:05 -0400

    Added feature extraction for power, fft power and entropy

--- ECE4553_Project.m
+++ ECE4553_Project.m
@@ -4,7 +4,7 @@
 % Authors: Ben W. & Chris T.
 % Date Created: Oct-30-2018
 % 
-% All data is from: https://www.nitrc.org/frs/?group_id=383
+% All data obtained from: https://www.nitrc.org/frs/?group_id=383
 
 %% Clearing
 
@@ -13,13 +13,12 @@ clc
 
 %% Import fMRI Image Data
 
-filename = 'mprage_noface.nii';
+%filename = 'ADHD200_parcellate_400.nii';
 
-brain_image_test = load_nii('mprage_noface.nii');
+brain_image_test = load_nii('ADHD200_parcellate_400.nii');
 
 view_nii(brain_image_test);
 
-%v = make_ana(brain_image_test);
 %% Loading 1D Data
 
 %{
@@ -31,10 +30,8 @@ The phenotype information is contained in an xlsx file created from the tsv
 the adhd200_preprocessed_phenotypics.tsv pheonotype file.
 %}
 
-clear name
-
 OneD_names = dir('OneD_data/*.txt');
-fMRI_data = cell(size(OneD_names,1),1);
+fMRI_data = cell(size(OneD_names,1),4);
 
 fMRI_info = importdata('adhd200_preprocessed_phenotypics.xlsx');
 
@@ -81,6 +78,16 @@ for j=1:size(fMRI_data,1)
     fMRI_data{j,1} = x(index,6);
 end
 
+for j=1:size(fMRI_data,1)
+    if (fMRI_data{j,1})
+        fMRI_data{j,4} = 1;
+        class(j,1) = 1;
+    else
+        fMRI_data{j,4} = 0;
+        class(j,1) = 0;
+    end
+end
+
 %% Clean Variables
 clear i
 clear index
@@ -99,10 +106,9 @@ clear temp_name
 clear seen_nonzero
 clear str_load
 
+%% Plotting
 
-%% Finding Good Features
-
-x = 1:size(fMRI_data{i,2},1);
+x = 1:size(fMRI_data{1,2},1);
 x =x';
 temp_plot = fMRI_data{1,2};
 temp_plot2 = fMRI_data{2,2};
@@ -112,4 +118,87 @@ for i=1:size(fMRI_data{1,2},2)
     plot(x, temp_plot(:,i), '-o');
     figure(2)
     plot(x, temp_plot2(:,i), '-o');
+    pause
 end
+
+%% Max Power and Location of Max Power
+
+clear pxx
+
+features = zeros(size(class,2),2);
+
+for i=1:size(class,1)
+    xx = fMRI_data{i,2};
+    pxx = pwelch(xx(:,1));
+    [p,l] = findpeaks(pxx(:,1), 'SortStr', 'descend');
+    features(i,1) = p(1);
+    features(i,2) = l(1);
+end
+
+%% Plotting for Max Power and Location of Max Power
+%{
+for i=1:size(x,1)
+    xx = x(:,i);
+    xx2 = x2(:,i);
+    pxx = pwelch(xx);
+    pxx2 = pwelch(xx2);
+    x_axis = 1:size(pxx,1);
+    x_axis';
+    figure(1)
+    plot(x_axis, pxx);
+    figure(2)
+    plot(x_axis, pxx2);
+    pause
+end
+%}
+
+%% FFT Power
+
+clear fft_feat
+
+features = zeros(size(class,1),1);
+
+for i=1:size(class,1)
+    pxx = fMRI_data{i,2};
+    fft = fftn(pxx(:,1));
+    power_fft = pwelch(fft);
+    [p,l] = findpeaks(power_fft(:,1), 'SortStr', 'descend');
+    features(i,3) = p(1);
+    features(i,4) = l(1);
+end
+ 
+
+%% Entropy
+
+clear se
+
+pxx = fMRI_data{i,2};
+se = zeros(size(class,1),size(pxx,2));
+
+for i=1:size(class,1)
+    pxx = fMRI_data{i,2};
+    for j=1:size(pxx,2)
+        se(i,j) = mean(pentropy(pxx(:,j),1));
+    end
+    i
+end
+
+%% Generating a Classifier
+
+LDA_power = fitcdiscr(features(:,5),class,'CrossVal','on');
+
+res = kfoldPredict(LDA_power);
+
+%% Accuracy
+
+clear acc
+
+total = 0;
+
+for i=1:size(class)
+   if(res(i) == class(i))
+       total = total + 1;
+   end 
+end
+
+acc = total/size(class,1);
